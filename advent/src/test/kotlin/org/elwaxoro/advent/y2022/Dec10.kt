fc15d2595b9f29c20f514e1416fc13c5929e2486
Emery <memery@figure.com>
2022-12-10 11:15:48 -0700

    2022-12-10

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec10.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec10.kt
@@ -8,60 +8,50 @@ import org.elwaxoro.advent.padTo
  */
 class Dec10 : PuzzleDayTester(10, 2022) {
 
-    data class CPU(val instructions: List<String>, val bullshit: (x: Int, cycleCount: Int) -> Unit) {
-        
-    }
-
     /**
-     * "During" means the value from the previous idx
+     * Capture x * cycleCount on really important cycles only
      */
-    override fun part1(): Any = load().let { instructions ->
-        var x = 1
+    override fun part1(): Any {
         val importantCycles = mutableListOf<Int>()
         val importantCounts = listOf(20, 60, 100, 140, 180, 220)
-        var cycleCount = 1
-        instructions.forEach { s ->
-
+        load().goCPUgo { x, cycleCount ->
             if (cycleCount in importantCounts) {
                 importantCycles.add(x * cycleCount)
             }
-            cycleCount++
-            if (s != "noop") {
-                if (cycleCount in importantCounts) {
-                    importantCycles.add(x * cycleCount)
-                }
-                cycleCount++ // plus costs 2 because rasins
-                x += s.split(" ").last().toInt() // set the x
-            }
         }
-        println(importantCycles)
-        importantCycles.sum()
-    } == 13860
+        return importantCycles.sum()// == 13860
+    }
 
-    override fun part2(): Any = load().let { instructions ->
-        var x = 1 // sprite starts out 0,1,2
-        val crt = (0..5).map { listOf('.').padTo(40).toMutableList() }
-        var cycleCount = 0
-        instructions.forEach { s ->
-            // begin: draw a sprite maybe
-            var crtPos = cycleCount % 40
-            crt[cycleCount / 40][crtPos] = if (crtPos in (x - 1..x + 1)) {
-                '#'
-            } else {
-                ' '
-            }
+    /**
+     * Time to create a replacement screen!
+     * ###  #### #  # ####  ##    ##  ##  ###
+     * #  #    # #  # #    #  #    # #  # #  #
+     * #  #   #  #### ###  #       # #    ###
+     * ###   #   #  # #    # ##    # #    #  #
+     * # #  #    #  # #    #  # #  # #  # #  #
+     * #  # #### #  # #     ###  ##   ##  ###
+     */
+    override fun part2(): Any {
+        val crt = (0..5).map { listOf(' ').padTo(40).toMutableList() }
+        load().goCPUgo { x, cycleCount ->
+            val crtRow = (cycleCount - 1) / 40
+            val crtPos = (cycleCount - 1) % 40
+            crt[crtRow][crtPos] = '#'.takeIf { crtPos in (x - 1..x + 1) }  ?: ' '
+        }
+        return "\n\n${crt.joinToString("\n") { it.joinToString("") }}"
+    }
+
+    private fun List<String>.goCPUgo(doSomeBullshit: (x: Int, cycleCount: Int) -> Unit) {
+        var x = 1
+        var cycleCount = 1
+        forEach { s ->
+            doSomeBullshit(x, cycleCount)
             cycleCount++
             if (s != "noop") {
-                crtPos = cycleCount % 40
-                crt[cycleCount / 40][crtPos] = if (crtPos in (x - 1..x + 1)) {
-                    '#'
-                } else {
-                    ' '
-                }
-                cycleCount++ // plus costs 2 because rasins
-                x += s.split(" ").last().toInt() // set the x
+                doSomeBullshit(x, cycleCount)
+                cycleCount++ // addx costs 2 because rasins
+                x += s.split(" ").last().toInt()
             }
         }
-        "\n\n${crt.joinToString("\n") { it.joinToString("") }}"
     }
 }
