915d144975073432da5ee6a287b0e69085db4db0
Emery <memery@figure.com>
2022-12-18 13:01:22 -0700

    2022-12-18

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec18.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec18.kt
@@ -4,31 +4,43 @@ import org.elwaxoro.advent.Coord3D
 import org.elwaxoro.advent.PuzzleDayTester
 import org.elwaxoro.advent.bounds
 
+/**
+ * Day 18: Boiling Boulders
+ */
 class Dec18: PuzzleDayTester(18, 2022) {
-    override fun part1(): Any = loader().let { coords ->
-        coords.sumOf { coord ->
-            6 - coords.count { that -> coord.manhattan(that) == 1 }
-        }
+
+    /**
+     * Pretty simple: for each lava block, count the neighbors that aren't also lava blocks
+     */
+    override fun part1(): Any = loader().let { lava ->
+        lava.sumOf { it.neighbors().minus(lava).size }
     }// == 4242
 
-    override fun part2(): Any = loader().let { lavaCoords ->
-        val bounds = lavaCoords.bounds(1)
-        val flooded = mutableSetOf<Coord3D>()
-        val contacts = mutableMapOf<Coord3D, Int>()
-        var active = setOf(bounds.min)
+    /**
+     * Just like minecraft! :D
+     * Fill the entire space with water blocks using BFS
+     * As water expands, check each block's neighbors for lava blocks and already visited water blocks
+     * Water blocks can touch multiple rocks at once, but each rock face may only be touched by a single water block
+     * Keep track of any water blocks that touch lava
+     */
+    override fun part2(): Any = loader().let { lava ->
+        val bounds = lava.bounds(1) // pad the lava bounds by 1 so water can get all the way around it
+        val flooded = mutableSetOf<Coord3D>() // visited blocks
+        val contacts = mutableMapOf<Coord3D, Int>() // water blocks that touched lava
+        var active = setOf(bounds.min) // water blocks that could still expand
         while(active.isNotEmpty()) {
-            active = active.flatMap { coord ->
-                val potential = coord.neighbors().filter {
+            active = active.flatMap { waterBlock ->
+                val neighbors = waterBlock.neighbors().filter {
                     bounds.contains(it) && !flooded.contains(it)
                 }
-                val rocks = potential.filter { lavaCoords.contains(it) }
-                if (rocks.isNotEmpty()) {
-                    contacts[coord] = rocks.size
+                val lavaNeighbors = neighbors.filter { lava.contains(it) }
+                if (lavaNeighbors.isNotEmpty()) {
+                    contacts[waterBlock] = lavaNeighbors.size
                 }
-                flooded.add(coord)
-                potential.minus(rocks)
+                flooded.add(waterBlock)
+                neighbors.minus(lavaNeighbors)
             }.toSet()
-            println("loop complete. flooded: ${flooded.size} active: ${active.size} rock touches: ${contacts.map { it.value }.sum()}")
+            // println("loop complete. flooded: ${flooded.size} active: ${active.size} rock touches: ${contacts.map { it.value }.sum()}")
         }
         contacts.map { it.value }.sum()
     }// == 2428
