2c08886129879334b5a126c0bb6914d3ec03efc8
Emery <memery@figure.com>
2022-12-24 16:09:49 -0700

    2022-12-23

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec23.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec23.kt
@@ -35,46 +35,22 @@ class Dec23 : PuzzleDayTester(23, 2022) {
         }.toSet()
     }
 
+    private fun List<KFunction2<Coord, Set<Coord>, Coord?>>.rotate() = takeSplit(1).let { (first, rest) -> rest.plus(first) }
+
+    private fun Set<Coord>.countSpace() = bounds().let { (min, max) ->
+        ((max.x - min.x + 1) * (max.y - min.y + 1)) - size
+    }
+
     override fun part1(): Any = loader().let { loaded ->
-//        println(loaded.printify(invert=true))
         var elves = loaded
-        var hasMoved = true
         var checks: List<KFunction2<Coord, Set<Coord>, Coord?>> = listOf(::checkNorth, ::checkSouth, ::checkWest, ::checkEast)
 
         (0 until 10).forEach { _ ->
-            val proposedMoves: MutableMap<Coord, MutableList<Coord>> = mutableMapOf()
-            elves.map { elf ->
-                val grid: Set<Coord> = elf.neighbors9().flatten().minus(elf).toSet()
-                val neighbors: Set<Coord> = grid.intersect(elves)
-                val p = if (neighbors.isEmpty()) {
-                    elf
-                } else {
-                    try {
-                        checks.firstNotNullOf {
-                            it.invoke(elf, neighbors)
-                        }
-                    } catch (ex: Exception) {
-                        elf
-                    }
-                }
-
-                proposedMoves.getOrPut(p){ mutableListOf() }.add(elf)
-            }
-            val newElves = proposedMoves.flatMap { (p,e) ->
-                if(e.size == 1) { listOf(p) } else { e }
-            }.toSet()
-            hasMoved = elves != newElves
-            elves = newElves
-            val (first, rest) = checks.takeSplit(1)
-            checks = rest.plus(first)
+            elves = loopOnce(elves, checks)
+            checks = checks.rotate()
         }
 
-        val (min, max) = elves.bounds()
-        val x = max.x - min.x + 1
-        val y = max.y - min.y + 1
-        val size = x * y
-        // 6241 is too high
-        size - elves.size
+        elves.countSpace()
     } == 3920
 
     override fun part2(): Any = loader().let { loaded ->
