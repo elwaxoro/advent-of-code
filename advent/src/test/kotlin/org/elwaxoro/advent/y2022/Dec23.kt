3bb26618110b5baa1f974c9b768c57dd18df8ed0
Emery <memery@figure.com>
2022-12-24 16:14:39 -0700

    2022-12-23

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec23.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec23.kt
@@ -4,7 +4,47 @@ import org.elwaxoro.advent.*
 import java.lang.Exception
 import kotlin.reflect.KFunction2
 
+/**
+ * Day 23: Unstable Diffusion
+ */
 class Dec23 : PuzzleDayTester(23, 2022) {
+
+    /**
+     * Loop through the elf search pattern 10 times, count the open spaces
+     * ~100ms
+     */
+    override fun part1(): Any = loader().let { loaded ->
+        var elves = loaded
+        var checks = listOf(::checkNorth, ::checkSouth, ::checkWest, ::checkEast)
+
+        (0 until 10).forEach { _ ->
+            elves = loopOnce(elves, checks)
+            checks = checks.rotate()
+        }
+
+        elves.countSpace()
+    }// == 3920
+
+    /**
+     * Loop through the elf search pattern until it stabilises
+     * ~2500ms
+     */
+    override fun part2(): Any = loader().let { loaded ->
+        var elves = loaded
+        var hasMoved = true
+        var counter = 0
+        var checks = listOf(::checkNorth, ::checkSouth, ::checkWest, ::checkEast)
+
+        while (hasMoved) {
+            counter++
+            val newElves = loopOnce(elves, checks)
+            hasMoved = elves != newElves
+            elves = newElves
+            checks = checks.rotate()
+        }
+
+        counter
+    }// == 889
 
     private fun checkNorth(elf: Coord, neighbors: Set<Coord>): Coord? = elf.move(Dir.N).takeIf { neighbors.none { it.y > elf.y } }
     private fun checkSouth(elf: Coord, neighbors: Set<Coord>): Coord? = elf.move(Dir.S).takeIf { neighbors.none { it.y < elf.y } }
@@ -28,10 +68,14 @@ class Dec23 : PuzzleDayTester(23, 2022) {
                 }
             }
 
-            proposedMoves.getOrPut(p){ mutableListOf() }.add(elf)
+            proposedMoves.getOrPut(p) { mutableListOf() }.add(elf)
         }
-        return proposedMoves.flatMap { (p,e) ->
-            if(e.size == 1) { listOf(p) } else { e }
+        return proposedMoves.flatMap { (p, e) ->
+            if (e.size == 1) {
+                listOf(p)
+            } else {
+                e
+            }
         }.toSet()
     }
 
@@ -41,57 +85,10 @@ class Dec23 : PuzzleDayTester(23, 2022) {
         ((max.x - min.x + 1) * (max.y - min.y + 1)) - size
     }
 
-    override fun part1(): Any = loader().let { loaded ->
-        var elves = loaded
-        var checks: List<KFunction2<Coord, Set<Coord>, Coord?>> = listOf(::checkNorth, ::checkSouth, ::checkWest, ::checkEast)
-
-        (0 until 10).forEach { _ ->
-            elves = loopOnce(elves, checks)
-            checks = checks.rotate()
-        }
-
-        elves.countSpace()
-    } == 3920
-
-    override fun part2(): Any = loader().let { loaded ->
-//        println(loaded.printify(invert=true))
-        var elves = loaded
-        var hasMoved = true
-        var counter = 0
-        var checks = listOf(::checkNorth, ::checkSouth, ::checkWest, ::checkEast)
-
-        while(hasMoved) {
-            counter++
-            val proposedMoves: MutableMap<Coord, MutableList<Coord>> = mutableMapOf()
-            elves.map { elf ->
-                val grid: Set<Coord> = elf.neighbors9().flatten().minus(elf).toSet()
-                val neighbors: Set<Coord> = grid.intersect(elves)
-                val p = if (neighbors.isEmpty()) {
-                    elf
-                } else {
-                    try {
-                        checks.firstNotNullOf {
-                            it.invoke(elf, neighbors)
-                        }
-                    } catch (ex: Exception) {
-                        elf
-                    }
-                }
-
-                proposedMoves.getOrPut(p){ mutableListOf() }.add(elf)
-            }
-            val newElves = proposedMoves.flatMap { (p,e) ->
-                if(e.size == 1) { listOf(p) } else { e }
-            }.toSet()
-            hasMoved = elves != newElves
-            elves = newElves
-            val (first, rest) = checks.takeSplit(1)
-            checks = rest.plus(first)
-        }
-
-        counter
-    } == 889
-
+    /**
+     * Reverse the input to make visualizing the coordinates easier
+     * Ignore empty space completely
+     */
     private fun loader() = load().reversed().flatMapIndexed { y, row ->
         row.mapIndexedNotNull { x, c -> Coord(x, y).takeIf { c == '#' } }
     }.toSet()
