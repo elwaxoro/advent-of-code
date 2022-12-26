c2bd82a5a13f6e10667dedfdfea820fb2ad030c8
Emery <memery@figure.com>
2022-12-25 17:25:36 -0700

    2022-12-22

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec22.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec22.kt
@@ -3,7 +3,10 @@ package org.elwaxoro.advent.y2022
 import org.elwaxoro.advent.*
 import kotlin.math.max
 
-class Dec22: PuzzleDayTester(22, 2022) {
+/**
+ * Day 22: Monkey Map
+ */
+class Dec22 : PuzzleDayTester(22, 2022) {
 
     private val empty = '_'
 
@@ -18,48 +21,39 @@ class Dec22: PuzzleDayTester(22, 2022) {
 //        println(scenario.map.flatten().plus(path.map { it.copyD('x') }).plus(scenario.start.copyD('S')).printify(empty = ' ', invert = true))
         scenario.moves.forEach { m ->
             dir = dir.turn(m.turn)
-//            println("Dir is $dir, moving ${m.dist}")
             (0 until m.dist).forEach { _ ->
                 var newPos = pos.move(dir)
-                var mapCoord = map[newPos.y][newPos.x]
-                newPos = if(mapCoord.d == empty) {
-//                    println("Need to wrap at $newPos ${mapCoord.d}")
-//                    if(newPos.x == 0 && newPos.y == 42) {
-//                        println(scenario.map.flatten().plus(path.map { it.copyD('x') }).plus(scenario.start.copyD('S')).plus(mapCoord.copyD('Z')).printify(empty = ' ', invert = true))
-//                        println("Row")
-////                        println(map[newPos.x])
-//                    }
-                    // fell off the edge, need to wrap to somewhere else
-                    when(dir) {
-                        Dir.N -> mapR[newPos.x].wrapToStart()//.also { println("NORTH WRAP") }
-                        Dir.S -> mapR[newPos.x].wrapToEnd()//.also { println("SOUTH WRAP") }
-                        Dir.E -> map[newPos.y].wrapToStart()//.also { println("EAST WRAP") }
-                        Dir.W -> map[newPos.y].wrapToEnd()//.also { println("WEST WRAP") }
+                var lookup = map[newPos.y][newPos.x]
+                newPos = if (lookup.d == empty) {
+                    when (dir) {
+                        Dir.N -> mapR[newPos.x].wrapToStart()
+                        Dir.S -> mapR[newPos.x].wrapToEnd()
+                        Dir.E -> map[newPos.y].wrapToStart()
+                        Dir.W -> map[newPos.y].wrapToEnd()
                     }
                 } else {
                     newPos
                 }
-                mapCoord = map[newPos.y][newPos.x]
-//                println("new map coord: ${mapCoord.d}")
-                if(mapCoord.d == '.') {
+                lookup = map[newPos.y][newPos.x]
+                if (lookup.d == '.') {
                     pos = newPos
                     path.add(pos)
-                } else {
-//                    println("can't move from $pos to $newPos ${newPos.d} because mapCoord says ${mapCoord} ${mapCoord.d}")
                 }
-
             }
         }
-        println("Ending position: $pos facing $dir")
-        val row = pos.y-1
+        println("Ending position: $pos facing $dir row is ${map.size-pos.y-1}")
+
+        val row = map.size - pos.y - 1
         val col = pos.x
-        val face = when(dir) {
+        val face = when (dir) {
             Dir.N -> 3
             Dir.S -> 1
             Dir.E -> 0
             Dir.W -> 2
         }
-        println(scenario.map.flatten().plus(scenario.start.copyD('S')).plus(path.map { it.copyD('x') }).plus(pos.copyD('Z')).printify(empty = ' ', invert = true))
+
+        //println(scenario.map.flatten().plus(scenario.start.copyD('S')).plus(path.map { it.copyD('x') }).plus(pos.copyD('Z')).plus(Coord(0, 0, '0')).printify(empty = ' ', invert = true))
+        // 77046 is too low
         (row * 1000) + (4 * col) + face
 
     }
@@ -74,25 +68,24 @@ class Dec22: PuzzleDayTester(22, 2022) {
         val maxX = splitMap.maxOf { it.length }
         val maxY = splitMap.size
         val maxLength = max(maxX, maxY)
-        val bottomRow: List<Coord> = (0 .. maxLength+1).map { Coord(it, 0, empty) }
-        val topRows: List<List<Coord>> = (maxY+1 until maxLength+maxLength-maxY+2).map { y -> (0 .. maxLength+1).map { x -> Coord(x, y, empty) }}
-        val coords = splitMap.reversed().mapIndexed {y, row ->
-            listOf(Coord(0, y+1, empty)).plus(
-            row.padEnd(maxLength, ' ').mapIndexed { x, c ->
-                if(c == '.' || c == '#') {
-                    Coord(x+1, y+1, c)
-                } else {
-                    Coord(x+1, y+1, empty)
-                }
-            }).plus(Coord(maxLength+1, y+1, empty))
+        val bottomRow: List<Coord> = (0..maxLength + 1).map { Coord(it, 0, empty) }
+        val topRows: List<List<Coord>> = (maxY + 1 until maxLength + maxLength - maxY + 2).map { y -> (0..maxLength + 1).map { x -> Coord(x, y, empty) } }
+        val coords = splitMap.reversed().mapIndexed { y, row ->
+            listOf(Coord(0, y + 1, empty)).plus(
+                row.padEnd(maxLength, ' ').mapIndexed { x, c ->
+                    if (c == '.' || c == '#') {
+                        Coord(x + 1, y + 1, c)
+                    } else {
+                        Coord(x + 1, y + 1, empty)
+                    }
+                }).plus(Coord(maxLength + 1, y + 1, empty))
         }
         val start = coords.last().first { it.d == '.' }
-        println("start: $start")
 
         var turn: Turn = Turn.R
         var dist = ""
         val parsedMoves = moves.mapNotNull { c ->
-            if(c.isDigit()) {
+            if (c.isDigit()) {
                 dist += c
                 null
             } else {
@@ -106,8 +99,6 @@ class Dec22: PuzzleDayTester(22, 2022) {
         finalCoords.add(bottomRow)
         finalCoords.addAll(coords)
         finalCoords.addAll(topRows)
-        println("Final coords Y: ${finalCoords.size}")
-        println("Final coords X: ${finalCoords[0].size}")
         Scenario(finalCoords, start, parsedMoves)
     }
 
