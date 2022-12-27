802e18e6919f5e85b994525d8cfa67798d13f379
Emery <memery@figure.com>
2022-12-27 10:50:00 -0700

    2022-12-24

---
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec24.kt
@@ -0,0 +1,132 @@
+package org.elwaxoro.advent.y2022
+
+import org.elwaxoro.advent.Coord
+import org.elwaxoro.advent.Dir
+import org.elwaxoro.advent.PuzzleDayTester
+import org.elwaxoro.advent.printify
+import java.lang.IllegalStateException
+
+/**
+ * Day 24: Blizzard Basin
+ */
+class Dec24: PuzzleDayTester(24, 2022) {
+    val bliz = listOf('>', 'v', '^', '<')
+
+    private fun seek(start: Coord, end: Coord, startBlizzard: List<Coord>, max: Coord): State {
+        var blizzard = startBlizzard
+        val states = mutableMapOf<Int, State>()
+        states[blizzard.plus(start).hashCode()] = State(start, listOf(blizzard), 0, listOf(start))
+        blizzard = blizzard.moveBlizzard(max)
+        var minutes = 1
+        var solutions = listOf<State>()
+        while(solutions.isEmpty()) {
+            states.values.filter { it.minutes == minutes-1 }.toList().forEach { state ->
+
+                val options = state.dude.neighbors().plus(state.dude).filterNot { blizzard.containsCoord(it) }.filter {
+                    it.inBounds(max) || it.equalsCoord(start) || it.equalsCoord(end)
+                }
+
+                options.forEach { option ->
+                    val hash = blizzard.plus(option).hashCode()
+                    val prev = states[hash]
+                    if (prev == null || prev.minutes > minutes) {
+                        states[hash] = State(option, state.blizzard.plusElement(blizzard), minutes, state.path.plus(option))
+                    }
+                }
+            }
+            solutions = states.values.filter { it.dude.equalsCoord(end) }
+            minutes++
+            blizzard = blizzard.moveBlizzard(max)
+        }
+        return solutions.single()
+    }
+
+    override fun part1(): Any = loader().let { coords ->
+        val startBlizzard = coords.filter { it.d in bliz }
+        var blizzard = startBlizzard
+        val max = Coord(coords.maxOf { it.x } - 1, coords.maxOf { it.y } - 1)
+        val start = Coord(1, max.y + 1, 'S')
+        val end = Coord(max.x, 0, 'E')
+        val valley = coords.cleanValley()
+        val states = mutableMapOf<Int, State>()
+        states[blizzard.plus(start).hashCode()] = State(start, listOf(blizzard), 0, listOf(start))
+        blizzard = blizzard.moveBlizzard(max)
+        var minutes = 1
+        var solutions = listOf<State>()
+        while(solutions.isEmpty()) {
+            states.values.filter { it.minutes == minutes-1 }.toList().forEach { state ->
+
+                val options = state.dude.neighbors().plus(state.dude).filterNot { blizzard.containsCoord(it) }.filter {
+                    it.inBounds(max) || it.equalsCoord(start) || it.equalsCoord(end)
+                }
+
+                options.forEach { option ->
+                    val hash = blizzard.plus(option).hashCode()
+                    val prev = states[hash]
+                    if (prev == null || prev.minutes > minutes) {
+                        states[hash] = State(option, state.blizzard.plusElement(blizzard), minutes, state.path.plus(option))
+                    }
+                }
+            }
+            solutions = states.values.filter { it.dude.equalsCoord(end) }
+            minutes++
+            blizzard = blizzard.moveBlizzard(max)
+        }
+        println("Found solutions! ${solutions.size}")
+        solutions.single().minutes
+    }
+
+    private fun replay(s: State, b: List<Coord>, max: Coord, valley: List<Coord>) {
+        var blizzard = b
+        s.path.forEachIndexed { index, coord ->
+            println("-------- minute $index violation? ${blizzard.containsCoord(coord)}-------------")
+            println(valley.plus(coord.copyD('X')).plus(blizzard).printify(invert = true))
+//            println("Comparison blizzard")
+//            println(valley.plus(s.blizzard[index]).printify(invert = true))
+            blizzard = blizzard.moveBlizzard(max)
+        }
+    }
+
+    private data class State(val dude: Coord, val blizzard: List<List<Coord>>, val minutes: Int, val path: List<Coord>)
+
+    private fun Coord.equalsCoord(coord: Coord) = x == coord.x && y == coord.y
+
+    private fun Iterable<Coord>.containsCoord(coord: Coord) = any { it.x == coord.x && it.y == coord.y }
+
+    private fun Set<Coord>.cleanValley() = map {
+        if(it.d in bliz) {
+            it.copyD('.')
+        } else {
+            it
+        }
+    }
+
+    private fun Coord.inBounds(max: Coord): Boolean = (x > 0 && x <= max.x && y > 0 && y <= max.y)
+
+    private fun List<Coord>.moveBlizzard(max: Coord) = map {
+        val n = when(it.d) { // proposed new coord
+            '>' -> it.move(Dir.E)
+            '<' -> it.move(Dir.W)
+            'v' -> it.move(Dir.S)
+            '^' -> it.move(Dir.N)
+            else -> throw IllegalStateException("WHAT ARE YOU DOING STEP-BLIZZARD? ${it.d}")
+        }
+        if(n.x < 1) {
+            Coord(max.x, n.y, n.d)
+        } else if(n.x > max.x) {
+            Coord(1, n.y, n.d)
+        } else if(n.y < 1) {
+            Coord(n.x, max.y, n.d)
+        } else if(n.y > max.y) {
+            Coord(n.x, 1, n.d)
+        } else {
+            n
+        }
+    }
+
+    override fun part2(): Any = "asdf"
+
+    private fun loader() = load().reversed().mapIndexed { y, row ->
+        row.mapIndexed { x, c -> Coord(x,y,c) }
+    }.flatten().toSet()
+}
