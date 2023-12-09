cc0a7e8d4bd164093fc53c47d1a743beb28d2cf9
Emery <memery@figure.com>
2023-12-09 11:01:42 -0700

    2023-12-09 Mirage Maintenance

--- advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec09.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec09.kt
@@ -7,18 +7,27 @@ class Dec09: PuzzleDayTester(9, 2023) {
     /**
      * 1868368343
      */
-    override fun part1(): Any = loader().map { line ->
-        generateHistory(line).last()
-    }.sum() == 1868368343L
+    override fun part1(): Any = loader().sumOf { line ->
+        predictor9000(line, listOf(), { it.last() }, { acc, l -> l + acc })
+    }
 
-    private fun generateHistory(line: List<Long>, lastNums: List<Long> = listOf()): List<Long> =
+    /**
+     * 1022
+     */
+    override fun part2(): Any = loader().sumOf { line ->
+        predictor9000(line, listOf(), { it.first() }, { acc, l -> l - acc })
+    }
+
+    /**
+     * Basic idea: recursively reduce the line by subtracting each item from the next in the list until everything is zeros (line)
+     * At each recursion: also keep track of either the last (part 1) or first (part 2) item in the input list (tracker)
+     * At base case: combine tracker back up to the top using the combiner function
+     */
+    private fun predictor9000(line: List<Long>, tracker: List<Long>, selector: (line: List<Long>) -> Long, combiner: (acc: Long, l: Long) -> Long): Long =
         if (line.all { it == 0L }) {
-            // base case! everything has been reduced to zero, time to build it back up
-            val diffList = mutableListOf(0L)
-            lastNums.reversed().fold(0L) { acc, l ->
-                (l + acc).also { diffList.add(it) }
+            tracker.reversed().fold(0L) { acc, l ->
+                combiner(acc, l)
             }
-            diffList
         } else {
             val diffs = line.mapIndexedNotNull { idx, l ->
                 if (idx == line.size - 1) {
@@ -27,30 +36,7 @@ class Dec09: PuzzleDayTester(9, 2023) {
                     line[idx+1]-l
                 }
             }
-            generateHistory(diffs, lastNums.plus(line.last()))
-        }
-
-    override fun part2(): Any = loader().map { line ->
-        generateHistory2(line).last()
-    }.sum()
-
-    private fun generateHistory2(line: List<Long>, firstNums: List<Long> = listOf()): List<Long> =
-        if (line.all { it == 0L }) {
-            // base case! everything has been reduced to zero, time to build it back up
-            val diffList = mutableListOf(0L)
-            firstNums.reversed().fold(0L) { acc, l ->
-                (l - acc).also { diffList.add(it) }
-            }
-            diffList
-        } else {
-            val diffs = line.mapIndexedNotNull { idx, l ->
-                if (idx == line.size - 1) {
-                    null
-                } else {
-                    line[idx+1]-l
-                }
-            }
-            generateHistory2(diffs, firstNums.plus(line.first()))
+            predictor9000(diffs, tracker.plus(selector(line)), selector, combiner)
         }
 
     private fun loader() = load().map { it.split(" ").map { it.toLong() } }
