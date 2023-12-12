e4d7dd02de10336544861b7bc2e8f4c17636799c
Emery <memery@figure.com>
2023-12-12 12:21:28 -0700

    2023-12-12 Hot Springs

---
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec12.kt
@@ -0,0 +1,91 @@
+package org.elwaxoro.advent.y2023
+
+import org.elwaxoro.advent.PuzzleDayTester
+
+/**
+ * Hot Springs
+ */
+class Dec12: PuzzleDayTester(12, 2023) {
+
+    /**
+     * idea: first: check if record + checksum is impossible get next char in record.
+     * if ., drop it (recursive step)
+     * if ?, replace with both . and # (recursive step)
+     * if #, take next checksum item and convert things to # until item is satisfied (hitting a . before satisfied = broken attempt, give up)
+     * if next char after satisfied item is #, give up
+     * if next char after satisfied item is ? convert that to .
+     * if record is empty but checksum has items, give up
+     * if checksum is empty but record still has #, give up
+     */
+    override fun part1(): Any = loader().sumOf { (record, checksum) ->
+        recursiveSpringalator6001(record, checksum)
+    }
+
+    override fun part2() = loader().sumOf { (record, checksum) ->
+        recursiveSpringalator6001(record.repeatSeparator(5), checksum.repeat(5))
+    }
+
+    private fun String.repeatSeparator(size: Int): String = (0 until size).map { this + "?" }.joinToString("").dropLast(1)
+
+    private fun List<Int>.repeat(size: Int): List<Int> = (0 until size).flatMap { this }
+
+    private val memo = mutableMapOf<String, Long>()
+
+    //selector: (line: List<Long>) -> Long
+    private fun computeIfAbsent(record: String, checksum: List<Int>, compute: (record: String, checksum: List<Int>) -> Long): Long {
+        val key = record + checksum.joinToString()
+        if (memo.containsKey(key)) {
+            return memo.getValue(key)
+        } else {
+            val result = compute.invoke(record, checksum)
+            memo[key] = result
+            return result
+        }
+    }
+
+    private fun recursiveSpringalator6001(record: String, checksum: List<Int>): Long = computeIfAbsent(record, checksum) { _, _ ->
+        if (record.isEmpty() && checksum.isEmpty()) {
+            1 // base case: success! out of records and the checksum is empty
+        } else if (record.isEmpty() && checksum.isNotEmpty()) {
+            0 // base case: failure. out of records but still checksums to check
+        } else if (record.first() == '.') {
+            recursiveSpringalator6001(record.drop(1), checksum) // recursive case: strip the . and go again
+        } else if (record.first() == '?') {
+            recursiveSpringalator6001(record.drop(1), checksum) + // recursive case: same as replacing with a '.' that gets dropped next recursion
+                    (recursiveSpringalator6001('#' + record.drop(1), checksum).takeIf { checksum.isNotEmpty() } ?: 0L) // recursive case: attempt to start a set of springs, if any of the checksum is left
+        } else if(record.first() == '#') {
+            if (checksum.isEmpty()) {
+                0 // base case: invalid record: ran out of checksums but still have springs
+            } else {
+                val item = checksum.first()
+                if (record.length < item) {
+                    0 // base case: invalid record: remaining record isn't big enough for the checksum
+                } else {
+                    val testing = record.take(item)
+                    val remainder = record.drop(item).let {
+                        if (it.isNotEmpty() && it.first() == '?') {
+                            '.' + it.drop(1)
+                        } else {
+                            it
+                        }
+                    }
+                    if (testing.all { it == '#' || it == '?' } && (remainder.isEmpty() || remainder.first() == '.')) {
+                        // success! this checksum item matched the record correctly
+                        // recurse without the matched record or the checksum item
+                        recursiveSpringalator6001(remainder, checksum.drop(1))
+                    } else {
+                        // base case: invalid record: either the testing chunk couldn't be converted to springs or the next char after the chunk is also a spring
+                        0
+                    }
+                }
+            }
+        } else {
+            0 // idk just give up man this ain't workin
+        }
+    }
+
+    private fun loader() = load().map { line ->
+        val (record, checksum) = line.split(" ")
+        record to checksum.split(",").map { it.toInt() }
+    }
+}
