fa6440d16879bdc93cbadef0a1aefa21636da014
Emery <memery@figure.com>
2023-12-12 12:52:16 -0700

    2023-12-12 Hot Springs

--- advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec12.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec12.kt
@@ -17,6 +17,9 @@ class Dec12 : PuzzleDayTester(12, 2023) {
 
     private val memo = mutableMapOf<String, Long>()
 
+    /**
+     * HashMap was throwing ConcurrentModificationException so just make my own. with blackjack! and hookers!
+     */
     private fun computeIfAbsent(key: String, compute: () -> Long): Long =
         if (memo.containsKey(key)) {
             memo.getValue(key)
@@ -27,17 +30,11 @@ class Dec12 : PuzzleDayTester(12, 2023) {
         }
 
     private fun recursiveSpringalator6001(record: String, checksum: List<Int>): Long = computeIfAbsent(record + checksum.joinToString()) {
-        if (record.isEmpty() && checksum.isEmpty()) {
-            1 // base case: success! out of records and the checksum is empty
-        } else if (record.isEmpty() && checksum.isNotEmpty()) {
-            0 // base case: failure. out of records but still checksums to check
-        } else if (record.first() == '.') {
-            recursiveSpringalator6001(record.drop(1), checksum) // recursive case: strip the . and go again
-        } else if (record.first() == '?') {
-            // recursive case: try the ? as # and .
-            listOf('.', '#').sumOf { recursiveSpringalator6001(it + record.drop(1), checksum) }
-        } else if (record.first() == '#') {
-            if (checksum.isEmpty()) {
+        when (record.firstOrNull()) {
+            null -> 1L.takeIf { checksum.isEmpty() } ?: 0 // base case: out of records to check
+            '.' -> recursiveSpringalator6001(record.drop(1), checksum) // recursive step: throw away the .
+            '?' -> listOf('.', '#').sumOf { recursiveSpringalator6001(it + record.drop(1), checksum) } // recursive step: try # and .
+            '#' -> if (checksum.isEmpty()) {
                 0 // base case: invalid record: ran out of checksums but still have springs
             } else {
                 val item = checksum.first()
@@ -45,25 +42,18 @@ class Dec12 : PuzzleDayTester(12, 2023) {
                     0 // base case: invalid record: remaining record isn't big enough for the checksum
                 } else {
                     val testing = record.take(item)
-                    val remainder = record.drop(item).let {
-                        if (it.isNotEmpty() && it.first() == '?') {
-                            '.' + it.drop(1)
-                        } else {
-                            it
-                        }
-                    }
-                    if (testing.all { it == '#' || it == '?' } && (remainder.isEmpty() || remainder.first() == '.')) {
-                        // success! this checksum item matched the record correctly
-                        // recurse without the matched record or the checksum item
+                    // if the remainder starts with a '?' swap it for a '.'
+                    val remainder = record.drop(item).let { it.takeUnless { it.firstOrNull() == '?' } ?: ('.' + it.drop(1)) }
+
+                    if (testing.all { it == '#' || it == '?' } && remainder.firstOrNull () != '#') {
+                        // recursive step: this all looks good so keep going
                         recursiveSpringalator6001(remainder, checksum.drop(1))
                     } else {
-                        // base case: invalid record: either the testing chunk couldn't be converted to springs or the next char after the chunk is also a spring
-                        0
+                        0 // base case: either the testing chunk couldn't be converted to springs or the next char after the chunk is also a spring
                     }
                 }
             }
-        } else {
-            0 // idk just give up man this ain't workin
+            else -> 0 // base case: idk
         }
     }
 
