00a981227384e0f8434a3c470c20dc5659a5b973
Emery <memery@figure.com>
2023-12-12 13:05:41 -0700

    2023-12-12 Hot Springs

--- advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec12.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec12.kt
@@ -7,31 +7,16 @@ import org.elwaxoro.advent.PuzzleDayTester
  */
 class Dec12 : PuzzleDayTester(12, 2023) {
 
-    override fun part1(): Any = loader().sumOf { (record, checksum) -> recursiveSpringalator6001(record, checksum) } == 7344L
+    /**
+     * 7344L
+     */
+    override fun part1(): Any = loader().sumOf { (record, checksum) -> recursiveSpringalator6001(record, checksum) }
 
     /**
-     * same as part 1, but taking forever to complete
-     * add memoization, now it's fast again!
+     * 1088006519007L
+     * same as part 1, but taking forever to complete. add memoization: now it's fast again!
      */
-    override fun part2() = loader().sumOf { (record, checksum) -> recursiveSpringalator6001(record.repeatSeparator(5), checksum.repeat(5)) } == 1088006519007L
-
-    private fun String.repeatSeparator(size: Int): String = (0 until size).joinToString("") { "$this?" }.dropLast(1)
-
-    private fun List<Int>.repeat(size: Int): List<Int> = (0 until size).flatMap { this }
-
-    private val memo = mutableMapOf<String, Long>()
-
-    /**
-     * HashMap was throwing ConcurrentModificationException so just make my own. with blackjack! and hookers!
-     */
-    private fun computeIfAbsent(key: String, compute: () -> Long): Long =
-        if (memo.containsKey(key)) {
-            memo.getValue(key)
-        } else {
-            val result = compute.invoke()
-            memo[key] = result
-            result
-        }
+    override fun part2() = loader().sumOf { (record, checksum) -> recursiveSpringalator6001(record.repeatSeparator(5), checksum.repeat(5)) }
 
     private fun recursiveSpringalator6001(record: String, checksum: List<Int>): Long = computeIfAbsent(record + checksum.joinToString()) {
         when (record.firstOrNull()) {
@@ -56,4 +41,23 @@ class Dec12 : PuzzleDayTester(12, 2023) {
         val (record, checksum) = line.split(" ")
         record to checksum.split(",").map { it.toInt() }
     }
+
+    private fun String.repeatSeparator(size: Int): String = (0 until size).joinToString("") { "$this?" }.dropLast(1)
+
+    private fun List<Int>.repeat(size: Int): List<Int> = (0 until size).flatMap { this }
+
+    // doesn't matter if the cache from part 1 is mixed with part 2
+    private val memo = mutableMapOf<String, Long>()
+
+    /**
+     * HashMap was throwing ConcurrentModificationException so just make my own. with blackjack! and hookers!
+     */
+    private fun computeIfAbsent(key: String, compute: () -> Long): Long =
+        if (memo.containsKey(key)) {
+            memo.getValue(key)
+        } else {
+            val result = compute.invoke()
+            memo[key] = result
+            result
+        }
 }
