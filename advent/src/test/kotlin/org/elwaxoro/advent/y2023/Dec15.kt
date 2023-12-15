925afb741d252c006ac615e219fbe2c61e5b2c5d
Emery <memery@figure.com>
2023-12-15 11:07:32 -0700

    2023-12-15 Lens Library

--- advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec15.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec15.kt
@@ -10,38 +10,36 @@ class Dec15 : PuzzleDayTester(15, 2023) {
     /**
      * 506437
      */
-    override fun part1(): Any = loader().sumOf { it.HASH() } == 506437
+    override fun part1(): Any = loader().sumOf { it.HASH() }
 
     /**
      * 288521
      */
-    override fun part2(): Any =
-        (0..<256).map { mutableListOf<Pair<String, Int>>() }.also { HASHMAP ->
-            loader().forEach { input ->
-                val label = input.replace("[\\-=0-9]*".toRegex(), "")
-                val bucket = HASHMAP[label.HASH()]
-                if (input.contains('-')) {
-                    bucket.removeAll { it.first == label }
-                } else {
-                    val upsert = label to input.takeLast(1).toInt()
-                    if (bucket.any { it.first == label }) {
-                        bucket.replaceAll {
-                            if (it.first == label) {
-                                upsert
-                            } else {
-                                it
-                            }
-                        }
+    override fun part2(): Any = loader().fold((0..<256).map { mutableListOf<Pair<String, Int>>() }) { HASHMAP, input ->
+        val label = input.replace("[\\-=0-9]*".toRegex(), "")
+        val bucket = HASHMAP[label.HASH()]
+        if (input.contains('-')) {
+            bucket.removeAll { it.first == label }
+        } else {
+            val upsert = label to input.takeLast(1).toInt()
+            if (bucket.any { it.first == label }) {
+                bucket.replaceAll {
+                    if (it.first == label) {
+                        upsert
                     } else {
-                        bucket.add(upsert)
+                        it
                     }
                 }
+            } else {
+                bucket.add(upsert)
             }
-        }.flatMapIndexed { key, value ->
-            value.mapIndexed { index, pair ->
-                (key + 1) * (index + 1) * pair.second
-            }
-        }.sum() == 288521
+        }
+        HASHMAP
+    }.flatMapIndexed { key, value ->
+        value.mapIndexed { index, pair ->
+            (key + 1) * (index + 1) * pair.second
+        }
+    }.sum()
 
     private fun String.HASH(): Int = fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
 
