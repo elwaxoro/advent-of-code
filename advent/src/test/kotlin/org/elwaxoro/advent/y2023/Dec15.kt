536890a1b5f462b0526e29c33bed59ed028baf01
Emery <memery@figure.com>
2023-12-15 11:03:18 -0700

    2023-12-15 Lens Library

--- advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec15.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2023/Dec15.kt
@@ -5,46 +5,45 @@ import org.elwaxoro.advent.PuzzleDayTester
 /**
  * Lens Library
  */
-class Dec15: PuzzleDayTester(15, 2023) {
+class Dec15 : PuzzleDayTester(15, 2023) {
 
     /**
      * 506437
      */
-    override fun part1(): Any = loader().map { it.reindeerHASH() }.sum() == 506437
+    override fun part1(): Any = loader().sumOf { it.HASH() } == 506437
 
     /**
      * 288521
      */
-    override fun part2(): Any = loader().let { inputs ->
-        val reindeerHASHMAP = (0..<256).map { mutableListOf<Pair<String, Int>>() }
-        inputs.forEach { input ->
-            val label = input.replace("[\\-=0-9]*".toRegex(), "")
-            val labelHASH = label.reindeerHASH()
-            if (input.contains('-')) {
-                reindeerHASHMAP[labelHASH].removeAll { it.first == label }
-            } else {
-                val focalLength = input.takeLast(1).toInt()
-                if (reindeerHASHMAP[labelHASH].any { it.first == label }) {
-                    reindeerHASHMAP[labelHASH].replaceAll {
-                        if (it.first == label) {
-                            it.first to focalLength
-                        } else {
-                            it
-                        }
-                    }
+    override fun part2(): Any =
+        (0..<256).map { mutableListOf<Pair<String, Int>>() }.also { HASHMAP ->
+            loader().forEach { input ->
+                val label = input.replace("[\\-=0-9]*".toRegex(), "")
+                val bucket = HASHMAP[label.HASH()]
+                if (input.contains('-')) {
+                    bucket.removeAll { it.first == label }
                 } else {
-                    reindeerHASHMAP[labelHASH].add(label to focalLength)
+                    val upsert = label to input.takeLast(1).toInt()
+                    if (bucket.any { it.first == label }) {
+                        bucket.replaceAll {
+                            if (it.first == label) {
+                                upsert
+                            } else {
+                                it
+                            }
+                        }
+                    } else {
+                        bucket.add(upsert)
+                    }
                 }
             }
-        }
-        reindeerHASHMAP.flatMapIndexed { key, value ->
+        }.flatMapIndexed { key, value ->
             value.mapIndexed { index, pair ->
                 (key + 1) * (index + 1) * pair.second
             }
-        }.sum()
-    }
+        }.sum() == 288521
 
-    private fun String.reindeerHASH(): Int = fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
+    private fun String.HASH(): Int = fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
 
     private fun loader() = load().single().split(",")
 }
