2108c14d6498ee484f6e7529bcbeaf75a338fe50
Emery <memery@figure.com>
2024-12-06 12:07:19 -0700

    dec 6 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec06.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec06.kt
@@ -5,20 +5,18 @@ import org.elwaxoro.advent.*
 /**
  * Day 6: Guard Gallivant
  */
-class Dec06: PuzzleDayTester(6, 2024) {
+class Dec06 : PuzzleDayTester(6, 2024) {
 
-    override fun part1(): Any = loader().let { (guard, objects) ->
-        guard.moveTillExitOrLoop(objects)
+    override fun part1(): Any = loader().let { (guard, obstacles) ->
+        guard.moveTillExitOrLoop(obstacles)
         guard.path.map { it.first }.toSet().size - 1
     }
 
-    override fun part2(): Any = loader().let { (guard, objects) ->
-        val start = guard.c
-        guard.moveTillExitOrLoop(objects)
-        val testObstacles = guard.path.map { it.first }.toSet().minus(start)
-        testObstacles.filter { ob ->
-            val g = Guard(start)
-            g.moveTillExitOrLoop(objects.plus(ob))
+    override fun part2(): Any = loader().let { (guard, obstacles) ->
+        guard.moveTillExitOrLoop(obstacles)
+        guard.path.map { it.first }.drop(1).toSet().filter { testObstacle ->
+            // takes about a minute to run without preload, 15 seconds with. This still isn't super efficient but meh
+            Guard.preload(guard.path.take(guard.path.indexOfFirst { it.first == testObstacle })).moveTillExitOrLoop(obstacles + testObstacle)
         }.size - 1
     }
 
@@ -27,9 +25,13 @@ class Dec06: PuzzleDayTester(6, 2024) {
         var d: Dir = Dir.N,
         val path: MutableList<Pair<Coord, Dir>> = mutableListOf(c to d)
     ) {
-        fun move(objects: Set<Coord>): Boolean {
+        companion object {
+            fun preload(p: List<Pair<Coord, Dir>>): Guard = Guard(p.last().first, p.last().second, p.toMutableList())
+        }
+
+        fun move(obstacles: Set<Coord>): Boolean {
             val nc = c.move(d)
-            return if (objects.contains(nc)) {
+            return if (obstacles.contains(nc)) {
                 d = d.turn(Turn.R)
                 false
             } else {
@@ -43,10 +45,10 @@ class Dec06: PuzzleDayTester(6, 2024) {
             }
         }
 
-        fun moveTillExitOrLoop(objects: Set<Coord>): Boolean {
-            val bounds = objects.bounds()
-            while(bounds.contains(c)) {
-                val loop = move(objects)
+        fun moveTillExitOrLoop(obstacles: Set<Coord>): Boolean {
+            val bounds = obstacles.bounds()
+            while (bounds.contains(c)) {
+                val loop = move(obstacles)
                 if (loop) {
                     return true
                 }
@@ -57,7 +59,7 @@ class Dec06: PuzzleDayTester(6, 2024) {
 
     private fun loader() = load().let { lines ->
         var guard = Coord(0, 0, '?')
-        val coords = lines.mapIndexed { yee, line ->
+        val obstacles = lines.mapIndexed { yee, line ->
             val y = lines.size - yee
             line.mapIndexedNotNull { x, c ->
                 when (c) {
@@ -67,6 +69,6 @@ class Dec06: PuzzleDayTester(6, 2024) {
                 }
             }
         }.flatten()
-        Guard(guard) to coords.toSet()
+        Guard(guard) to obstacles.toSet()
     }
 }
