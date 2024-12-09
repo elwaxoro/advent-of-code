848df01d4b7161a11cfdb885e89f9bcfb929803e
Emery <memery@figure.com>
2024-12-09 11:57:57 -0700

    dec 9 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec09.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec09.kt
@@ -2,73 +2,63 @@ package org.elwaxoro.advent.y2024
 
 import org.elwaxoro.advent.PuzzleDayTester
 import org.elwaxoro.advent.splitToInt
-import org.elwaxoro.advent.toward
 
+/**
+ * Day 9: Disk Fragmenter
+ * Basic idea: expand disk so that it's full of -1s for empty space or repeated ids for filled space
+ * Use list of int so ids can go over 9
+ * Half expected part 2 to run forever or blow up mem, but it completes in about 2.5s
+ */
 class Dec09 : PuzzleDayTester(9, 2024) {
 
-    override fun part1(): Any = loader().expand().flatten().toMutableList().let { disk ->
-        while (disk.contains(-1)) {
+    /**
+     * trim the data from the end until no more -1s exist
+     * unlike the example, trim the list so the end never contains free space (gets smaller as it sorts)
+     */
+    override fun part1(): Any = load().single().splitToInt().expand().flatten().toMutableList().also { disk ->
+        var idx = disk.indexOf(-1)
+        do {
             while (disk.last() == -1) {
-                disk.removeLast()
+                disk.removeLast() // make sure there's no free space at the end
             }
-            val swap = disk.indexOf(-1)
-            disk[swap] = disk.removeLast()
-        }
-        disk.checksum()
-    }// == 6330095022244
-
-    private fun List<Int>.checksum(): Long = mapIndexedNotNull { idx, id ->
-        (idx * id.toLong()).takeUnless { id == -1 }
-    }.sum()
-
-    private fun List<Int>.expand(): List<List<Int>> = mapIndexed { idx, i ->
-        val fill = if (idx % 2 == 0) {
-            idx / 2
-        } else {
-            -1
-        }
-        (1..i).map { fill }
-    }
-
-    private fun List<Int>.findSpace(space: Int): Int {
-        forEachIndexed { idx, i ->
-            if (i == -1 && idx + space < size && (idx..<idx+space).all { this[it] == -1 }) {
-                return idx
+            if (disk[idx] == -1) {
+                disk[idx] = disk.removeLast()
             }
-        }
-        return -1
-    }
+            idx++
+        } while (idx < disk.size)
+    }.checksum()
 
-    override fun part2(): Any = loader().expand().let { disk ->
+    override fun part2(): Any = load().single().splitToInt().expand().let { disk ->
         val defrag = disk.flatten().toMutableList()
-        (disk.size -1 downTo 1).forEach { idx ->
-            val working = disk[idx]
-            if (working.isNotEmpty() && working[0] != -1) {
-                val a = defrag.indexOfFirst { it == working[0] }
-                val b = defrag.indexOfLast { it == working[0] }
-                val size = working.size
-                val target = defrag.findSpace(size)
-                if (target > -1 && target < a) {
-//                    println("working with $idx: $working. found space at $target")
-                    (a..b).forEach { defrag[it] = -1 }
-                    (target..<target+size).forEach { defrag[it] = working[0] }
-                } else {
-//                    println("working with $idx: $working. can't find anywhere to put it")
+        (disk.size - 1 downTo 1).forEach { idx ->
+            val chunk = disk[idx]
+            if (chunk.isNotEmpty() && chunk[0] != -1) {
+                val dataIdx = defrag.indexOfFirst { it == chunk[0] }
+                val emptyIdx = defrag.findSpace(chunk.size, dataIdx)
+                if (emptyIdx > -1 && emptyIdx < dataIdx) {
+                    (dataIdx..<dataIdx + chunk.size).forEach { defrag[it] = -1 }
+                    (emptyIdx..<emptyIdx + chunk.size).forEach { defrag[it] = chunk[0] }
                 }
-//                println(defrag.printify())
             }
         }
         defrag.checksum()
     }
 
-    private fun List<Int>.printify(): String =
-        map {
-            if (it == -1) {
-                "."
-            } else {
-                "$it"
-            }
-        }.joinToString("")
+    /**
+     * expands the disk into a list of chunks where each chunk has repeated id (or -1 for empty) for the size
+     * ex: 12345 becomes [[0], [-1, -1], [1, 1, 1], [-1, -1, -1, -1], [2, 2, 2, 2, 2]]
+     * thought this would matter but everything ended up flattened anyway. still kinda sorta helpful in part 2
+     */
+    private fun List<Int>.expand(): List<List<Int>> = mapIndexed { idx, size -> ((idx / 2).takeIf { idx % 2 == 0 } ?: -1).let { fill -> (1..size).map { fill } } }
 
-    private fun loader() = load().single().splitToInt()
+    private fun List<Int>.checksum(): Long = mapIndexedNotNull { idx, id -> (idx * id.toLong()).takeUnless { id == -1 } }.sum()
+
+    private fun List<Int>.findSpace(space: Int, maxIdx: Int): Int {
+        (0..maxIdx).forEach { idx ->
+            if (this[idx] == -1 && (idx..<idx + space).all { this[it] == -1 }) {
+                return idx
+            }
+        }
+        return -1
+    }
 }
