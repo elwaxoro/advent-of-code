9c2222acd8eccd05ca7bc944f7300ca309914bf9
Emery <memery@figure.com>
2024-12-08 18:40:14 -0700

    dec 8 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec08.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec08.kt
@@ -10,46 +10,35 @@ import org.elwaxoro.advent.contains
  */
 class Dec08 : PuzzleDayTester(8, 2024) {
 
-    override fun part1(): Any = loader().let { (antennas, bounds) ->
-        antennas.values.map { v ->
-            v.mapIndexed { idx, a ->
-                v.drop(idx + 1).flatMap { b ->
-                    val dxDy = a.dxdy(b)
-                    listOf(
-                        a.extend(b, dxDy),
-                        b.extend(a, dxDy, -1)
-                    )
-                }
-            }.flatten()
-        }.flatten().distinct().filter { bounds.contains(it) }.size
+    override fun part1(): Any = loader().makeAntinodes { a, b ->
+        val dxDy = a.dxdy(b)
+        listOf(
+            a.extend(b, dxDy),
+            b.extend(a, dxDy, -1)
+        )
+    }.size
+
+    override fun part2(): Any = loader().let { input ->
+        input.makeAntinodes { a, b ->
+            val dxDy = a.dxdy(b)
+            // board is 50x50 so just run the pattern out 50 times in both directions and throw away whatever is out of bounds
+            (1..50).flatMap { mult ->
+                listOf(
+                    a.extend(b, dxDy, mult),
+                    b.extend(a, dxDy, -1 * mult)
+                )
+            }
+        }.plus(input.first.values.flatten()).distinct().size
     }
 
-    override fun part2(): Any = loader().let { (antennas, bounds) ->
-        antennas.values.map { v ->
-            v.mapIndexed { idx, a ->
-                v.drop(idx + 1).flatMap { b ->
-                    val dxDy = a.dxdy(b)
-                    // super lazy. board is 50x50 so just run the pattern out 50 times in both directions and throw away whatever is out of bounds
-                    (1..50).flatMap { mult ->
-                        listOf(
-                            a.extend(b, dxDy, mult),
-                            b.extend(a, dxDy, mult * -1)
-                        )
-                    }
-                }
-            }.flatten() + v.map { it.copyD() }
-        }.flatten().distinct().filter { bounds.contains(it) }.size
-    }
-
-    private fun Pair<Map<Char, List<Coord>>, Pair<Coord, Coord>>.makeAntinodes(extendomatic: (a: Coord, b: Coord) -> List<Coord>) {
+    private fun Pair<Map<Char, List<Coord>>, Pair<Coord, Coord>>.makeAntinodes(extendomatic: (a: Coord, b: Coord) -> List<Coord>) =
         first.values.map { v ->
             v.mapIndexed { idx, a ->
                 v.drop(idx + 1).flatMap { b ->
                     extendomatic(a, b)
                 }
-            }.flatten() + v.map { it.copyD() }
-        }.flatten().distinct().filter { second.contains(it) }.size
-    }
+            }.flatten()
+        }.flatten().distinct().filter { second.contains(it) }
 
     private fun Coord.extend(that: Coord, dxdy: Pair<Int, Int>, mag: Int = 1): Coord = Coord(that.x + (dxdy.first * mag), that.y + (dxdy.second * mag))
 
@@ -60,7 +49,7 @@ class Dec08 : PuzzleDayTester(8, 2024) {
             lines.mapIndexed { iy, line ->
                 val y = lines.size - iy
                 line.mapIndexed { x, c ->
-                    antennas.getOrPut(c) { mutableListOf() }.add(Coord(x, y, c))
+                    antennas.getOrPut(c) { mutableListOf() }.add(Coord(x, y))
                 }
             }
         }.let { antennas ->
