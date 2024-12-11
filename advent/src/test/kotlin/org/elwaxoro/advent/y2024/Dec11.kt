d1fabb1dfe866801a4d4434b13f859046696c439
Emery <memery@figure.com>
2024-12-11 10:08:16 -0700

    dec 11 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec11.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec11.kt
@@ -12,13 +12,13 @@ class Dec11 : PuzzleDayTester(11, 2024) {
      * Initially solved with an expanding string to store stones in the correct order
      * Part 2 revealed none of that was needed so refactored to just use part 2 solution with part 1
      */
-    override fun part1(): Any = loader().blink(25) == 199753L
+    override fun part1(): Any = loader().blink(25)
 
     /**
      * Despite the instructions stating "order is preserved",
      * the puzzle does not care about order and the stones never interact
      */
-    override fun part2(): Any = loader().blink(75) == 239413123020116L
+    override fun part2(): Any = loader().blink(75)
 
     private fun Map<String, Long>.blink(count: Int): Long = (1..count).fold(this) { stones, _ ->
         mutableMapOf<String, Long>().also { newStones ->
