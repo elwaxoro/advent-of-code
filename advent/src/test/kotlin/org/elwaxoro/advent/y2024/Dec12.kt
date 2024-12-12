a632093a1379c96408d754b101f2f2cc1425e19d
Emery <memery@figure.com>
2024-12-12 09:42:07 -0700

    dec 12 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec12.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec12.kt
@@ -1,6 +1,7 @@
 package org.elwaxoro.advent.y2024
 
 import org.elwaxoro.advent.Coord
+import org.elwaxoro.advent.Dir
 import org.elwaxoro.advent.PuzzleDayTester
 
 /**
@@ -14,8 +15,16 @@ class Dec12 : PuzzleDayTester(12, 2024) {
         } * set.size
     }
 
-    override fun part2(): Any {
-        return super.part2()
+    override fun part2(): Any = loader().organize().sumOf { set ->
+
+        val totalEdges = Dir.entries.sumOf { dir ->         // for each direction (N, S, E, W)
+            // find coords that have no set-neighbor in that direction, these are edges
+            val sides = set.filter { coord -> !set.contains(coord.move(dir)) }
+            // run splitSets to get un-connected edges
+            val edges = sides.splitSets()
+            edges.size
+        }
+        totalEdges * set.size
     }
 
     /**
@@ -24,21 +33,14 @@ class Dec12 : PuzzleDayTester(12, 2024) {
      * 2. For each coord of same letter, find existing sets containing any neighbors
      * 3. Merge those sets into one set, along with coord
      */
-    private fun List<Pair<Coord, Char>>.organize(): List<Set<Coord>> =
-        this.groupBy { it.second }.map { (_, group) ->
-            group.fold(mutableListOf<Set<Coord>>()) { sets, (c, _) ->
-                sets.also {
-                    val matchedSets = c.neighbors().flatMap { n -> sets.filter { it.contains(n) } }
-                    sets.removeAll(matchedSets)
-                    val newSet = setOf(c) + matchedSets.flatten()
-                    sets.add(newSet)
-                }
-            }
-        }.flatten()
+    private fun List<Pair<Coord, Char>>.organize(): List<Set<Coord>> = this.groupBy { it.second }.map { it.value.map { it.first } }.map { it.splitSets() }.flatten()
 
-    private fun List<List<Coord>>.organize(): List<Set<Coord>> {
-
-        return TODO()
+    private fun List<Coord>.splitSets(): List<Set<Coord>> = fold(mutableListOf()) { sets, c ->
+        sets.also {
+            val matchedSets = c.neighbors().flatMap { n -> sets.filter { it.contains(n) } }
+            sets.removeAll(matchedSets)
+            sets.add(setOf(c) + matchedSets.flatten())
+        }
     }
 
     private fun loader() = load().mapIndexed { y, line -> line.mapIndexed { x, c -> Coord(x, y) to c } }.flatten()
