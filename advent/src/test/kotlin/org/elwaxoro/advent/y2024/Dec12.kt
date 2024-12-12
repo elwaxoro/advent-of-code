7de8b1f0953b084399516c276dd2711f4dda44ef
Emery <memery@figure.com>
2024-12-12 09:18:08 -0700

    dec 12 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec12.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec12.kt
@@ -8,28 +8,22 @@ import org.elwaxoro.advent.PuzzleDayTester
  */
 class Dec12 : PuzzleDayTester(12, 2024) {
 
+    override fun part1(): Any = loader().organize().sumOf { set ->
+        set.sumOf { c ->
+            4 - c.neighbors().count { it in set }
+        } * set.size
+    }
+
+    override fun part2(): Any {
+        return super.part2()
+    }
+
     /**
      * Turbo-lazy idea:
      * 1. Group by letter, then just need to split up disconnected sets
      * 2. For each coord of same letter, find existing sets containing any neighbors
      * 3. Merge those sets into one set, along with coord
      */
-    override fun part1(): Any = loader().groupBy { it.second }.map { (_, group) ->
-        val sets = group.fold(mutableListOf<Set<Coord>>()) { sets, (c, _) ->
-            sets.also {
-                val matchedSets = c.neighbors().flatMap { n -> sets.filter { it.contains(n) } }
-                sets.removeAll(matchedSets)
-                val newSet = setOf(c) + matchedSets.flatten()
-                sets.add(newSet)
-            }
-        }
-            sets.sumOf { set ->
-            set.sumOf { c ->
-                4 - c.neighbors().count { it in set }
-            } * set.size
-        }
-    }.sum()
-
     private fun List<Pair<Coord, Char>>.organize(): List<Set<Coord>> =
         this.groupBy { it.second }.map { (_, group) ->
             group.fold(mutableListOf<Set<Coord>>()) { sets, (c, _) ->
@@ -42,9 +36,10 @@ class Dec12 : PuzzleDayTester(12, 2024) {
             }
         }.flatten()
 
-    private fun loader() = load().mapIndexed { y, line ->
-        line.mapIndexed { x, c ->
-            Coord(x, y) to c
-        }
-    }.flatten()
+    private fun List<List<Coord>>.organize(): List<Set<Coord>> {
+
+        return TODO()
+    }
+
+    private fun loader() = load().mapIndexed { y, line -> line.mapIndexed { x, c -> Coord(x, y) to c } }.flatten()
 }
