ab135e82b05c0660a53a46fd6d3387ce61640402
Emery <memery@figure.com>
2024-12-13 09:23:53 -0700

    dec 13 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec13.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec13.kt
@@ -9,51 +9,60 @@ import kotlin.math.roundToLong
 
 /**
  * Day 13: Claw Contraption
+ *
  * I WAS TOLD THERE WOULD BE NO MATH
+ *
+ * prize at p.x, p.y
+ * pap = presses of A button moving a.x, a.y each time
+ * bap = presses of B button moving b.x, b.y each time
+ * p.x = (pap * a.x) + (bap * b.x)
+ * p.y = (pap * a.y) + (bap * b.y)
+ *
+ * solve for pap
+ * p.x - (bap * b.x) = pap * a.x
+ * (p.x - (bap * b.x) / a.x = pap
+ * (p.x / a.x) - bap * (b.x / a.x) = pap
+ * (p.y / a.y) - bap * (b.y / a.y) = pap
+ *
+ * extract a couple variables
+ * pax = p.x / a.x
+ * pay = p.y / a.y
+ * bax = b.x / a.x
+ * bay = b.y / a.y
+ *
+ * ahhh much nicer
+ * pax - bap * bax = pap
+ * pay - bap * bay = pap
+ *
+ * solve for bap
+ * pax - bap * bax = pay - bap * bay
+ * pax - pay = bap * bax - bap * bay
+ * pax - pay = bap * (bax - bay)
+ * (pax - pay) / (bax - bay) = bap
+ *
+ * if bap and bay are "close enough" to whole numbers, the solve is good
  */
-class Dec13: PuzzleDayTester(13, 2024) {
-    // 78101482023732
-    // 72804396907562 is too low
-    // 28774107866398 is too low
-    val p2 = 10000000000000
+class Dec13 : PuzzleDayTester(13, 2024) {
 
-    override fun part1(): Any = loader().sumOf { (a, b, p) ->
-        val pax = (p.x+p2) / a.x.toDouble()
-        val pay = (p.y+p2) / a.y.toDouble()
+    override fun part1(): Any = loader().sumOf { it.solve() }
+    override fun part2(): Any = loader().sumOf { it.solve(10000000000000) }
+
+    private fun List<Coord>.solve(boost: Long = 0L): Long {
+        val (a, b, p) = this
+        val pax = (p.x + boost) / a.x.toDouble()
+        val pay = (p.y + boost) / a.y.toDouble()
         val bax = b.x / a.x.toDouble()
         val bay = b.y / a.y.toDouble()
-        val pp = pax - pay
-        val bb = bax - bay
-        val m = pp / bb // b button presses
-        val n = pay - (m * bay) // a button presses
-
-//        println("$a $b $p")
-//        if (m.isCloseToLong() && n.isCloseToLong()) {
-//            println("SOLVED!")
-//            println("Press A: $n = ${n.roundToLong()}")
-//            println("Press B: $m = ${m.roundToLong()}")
-//        } else {
-//            println("NO SOLUTION")
-//            println("press A: $n")
-//            println("press B: $m")
-//            println("N: ${n.roundToLong()}")
-//            println("M: ${m.roundToLong()}")
-//        }
-
-        if (m.isCloseToLong() && n.isCloseToLong()) {
-            val tokens = n.roundToLong() * 3 + m.roundToLong()
+        val paxy = pax - pay
+        val baxy = bax - bay
+        val bap = paxy / baxy
+        val pap = pay - (bap * bay)
+        return if (bap.isCloseToLong() && pap.isCloseToLong()) {
+            val tokens = pap.roundToLong() * 3 + bap.roundToLong()
             tokens
         } else {
             0
         }
-    }
-
-    override fun part2(): Any {
-        return super.part2()
-    }
-
-    private fun List<Coord>.solve(boost: Long = 1L): Long {
-        
     }
 
     private fun loader() = load(delimiter = "\n\n").map { game ->
