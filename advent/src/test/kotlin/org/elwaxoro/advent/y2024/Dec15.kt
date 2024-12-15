9b92f514c6508682e31336c785e70fc6dea083d8
Emery <memery@figure.com>
2024-12-15 13:38:00 -0700

    dec 15 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec15.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec15.kt
@@ -7,75 +7,74 @@ import org.elwaxoro.advent.*
  */
 class Dec15 : PuzzleDayTester(15, 2024) {
 
-    override fun part1(): Any = loader().let { w ->
-//        println(w.walls.copyD('#').plus(w.boxes.copyD('O')).plus(w.robot.copyD('R')).printify(invert = true))
-        w.moveAll()
-        println(w.walls.copyD('#').plus(w.boxes.copyD('O')).plus(w.robot.copyD('R')).printify(invert = true))
-        val (min, max) = w.walls.bounds()
-        w.boxes.sumOf {
-            it.x + (100 * (max.y - it.y))
+    override fun part1(): Any = loader(expand = false).runTheSimulation()
+
+    override fun part2() = loader(expand = true).runTheSimulation()
+
+    private data class Moveable(
+        val id: String,
+        var coords: Set<Coord>
+    ) {
+        fun move(dir: Dir) {
+            coords = coords.map { it.move(dir) }.toSet()
         }
     }
 
-    override fun part2(): Any {
-        return super.part2()
+    private data class Warehouse(
+        val walls: Set<Coord>,
+        val moveables: MutableMap<String, Moveable>,
+        val directions: List<Dir>
+    ) {
+
+        fun runTheSimulation(): Int {
+            directions.forEach { dir ->
+                val moved = move(dir, setOfNotNull(moveables["ROBOT"]))
+                if (moved.isNotEmpty()) {
+                    moved.forEach { m ->
+                        moveables[m.id]?.move(dir)
+                    }
+                }
+            }
+
+            val maxY = walls.maxY()
+            return moveables.minus("ROBOT").values.sumOf { m ->
+                m.coords.minX() + (100 * (maxY - m.coords.first().y))
+            }
+        }
+
+        fun move(dir: Dir, ms: Set<Moveable>): Set<Moveable> =
+            ms.flatMap { it.coords }.map { it.move(dir) }.let { check ->
+                if (check.any { walls.contains(it) }) {
+                    setOf()
+                } else {
+                    val newMs = ms.plus(moveables.values.filter { b -> check.any { b.coords.contains(it) } })
+                    if (newMs.size > ms.size) {
+                        move(dir, newMs)
+                    } else {
+                        ms
+                    }
+                }
+            }
     }
 
-    private fun loader() = load(delimiter = "\n\n").let { (grid, moves) ->
-        var robot = Coord(0, 0)
+    private fun loader(expand: Boolean) = load(delimiter = "\n\n").let { (grid, moves) ->
         val walls = mutableListOf<Coord>()
-        val boxes = mutableListOf<Coord>()
+        val movables = mutableMapOf<String, Moveable>()
         val gsplit = grid.split("\n")
         gsplit.mapIndexed { yidx, line ->
             val y = gsplit.size - yidx - 1
-            line.mapIndexed { x, c ->
+            line.mapIndexed { xidx, c ->
+                val x = (xidx * 2).takeIf { expand } ?: xidx
+                val coords = setOf(Coord(x, y), Coord(x + 1, y)).takeIf { expand } ?: setOf(Coord(x, y))
                 when (c) {
-                    '#' -> walls.add(Coord(x, y))
-                    'O' -> boxes.add(Coord(x, y))
-                    '@' -> robot = Coord(x, y)
+                    '#' -> walls.addAll(coords)
+                    'O' -> movables["Box$x,$y"] = Moveable("Box$x,$y", coords)
+                    '@' -> movables["ROBOT"] = Moveable("ROBOT", setOf(Coord(x, y)))
                     else -> {}
                 }
             }
         }
-        val dirs = moves.split("\n").flatMap { line -> line.map { Dir.fromCarets(it) } }
-        Warehouse(robot, walls.toSet(), boxes.toMutableSet(), dirs)
-    }
-
-    private data class Warehouse(
-        var robot: Coord,
-        val walls: Set<Coord>,
-        val boxes: MutableSet<Coord>,
-        val moves: List<Dir>,
-        var moveIdx: Int = 0,
-    ) {
-
-        fun moveAll() {
-            moves.forEach {
-                move(it)
-            }
-        }
-
-        fun move(dir: Dir) {
-            var c: Coord? = robot.move(dir)
-            val shift = mutableSetOf(robot)
-            while (c != null) {
-                if (walls.contains(c)) {
-                    // bonk. nobody moves
-                    shift.clear()
-                    c = null
-                } else if (boxes.contains(c)) {
-                    shift.add(c)
-                    c = c.move(dir)
-                } else {
-                    // just hit empty space, stop
-                    c = null
-                }
-            }
-            if (shift.isNotEmpty()) {
-                robot = robot.move(dir)
-                boxes.removeAll(shift)
-                boxes.addAll(shift.drop(1).map { it.move(dir) })
-            }
-        }
+        val directions = moves.split("\n").flatMap { line -> line.map { Dir.fromCarets(it) } }
+        Warehouse(walls.toSet(), movables, directions)
     }
 }
