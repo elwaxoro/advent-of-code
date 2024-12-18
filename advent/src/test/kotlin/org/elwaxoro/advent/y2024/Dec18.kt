4a67e25a896582d10659afd83a0f8bf351dbd1c1
Emery <memery@figure.com>
2024-12-18 10:39:16 -0700

    dec 18 2024

---
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec18.kt
@@ -0,0 +1,57 @@
+package org.elwaxoro.advent.y2024
+
+import org.elwaxoro.advent.Coord
+import org.elwaxoro.advent.PuzzleDayTester
+import org.elwaxoro.advent.contains
+import kotlin.Int.Companion.MAX_VALUE
+import kotlin.collections.set
+
+/**
+ * Day 18: RAM Run
+ */
+class Dec18 : PuzzleDayTester(18, 2024) {
+
+    // bounds contains the size of the grid AND represents start/end points
+    private val bounds = Pair(Coord(0, 0), Coord(70, 70))
+
+    override fun part1(): Any = explore(loader().take(1024))
+
+    override fun part2(): Any = loader().let { it[binaryExplore(it)] }
+
+    /**
+     * Feels like this might fail if the target byte is ever "mid" but this did not happen with my input
+     */
+    private fun binaryExplore(bytes: List<Coord>, low: Int = 0, high: Int = bytes.size): Int =
+        if (low == high) {
+            low
+        } else {
+            val mid = low + (high - low) / 2
+            val score = explore(bytes.take(mid))
+            if (score < MAX_VALUE) {
+                binaryExplore(bytes, mid + 1, high)
+            } else {
+                binaryExplore(bytes, low, mid - 1)
+            }
+        }
+
+    /**
+     * Basically a copy of Dec16 with extras torn out
+     */
+    private fun explore(bytes: List<Coord>): Int {
+        val explore = mutableListOf(bounds.first)
+        val cost = mutableMapOf(bounds.first to 0)
+        while (explore.isNotEmpty()) {
+            val ex = explore.removeFirst()
+            if (ex != bounds.second) {
+                val c = (cost[ex] ?: 0) + 1
+                ex.neighbors().filter { it !in bytes && bounds.contains(it) && (cost[it] ?: MAX_VALUE) > c }.map {
+                    explore.add(it)
+                    cost[it] = c
+                }
+            }
+        }
+        return cost[bounds.second] ?: MAX_VALUE
+    }
+
+    private fun loader() = load().map { Coord.parse(it) }
+}
