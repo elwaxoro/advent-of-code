980090468de977772bf9314c3c63bc08e3d25992
Emery <memery@figure.com>
2024-12-19 08:53:45 -0700

    dec 19 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec19.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec19.kt
@@ -4,34 +4,22 @@ import org.elwaxoro.advent.PuzzleDayTester
 
 /**
  * Day 19: Linen Layout
+ * HA! the memoize code here is a literal copy/paste from Dec 12 2023, the hot springs puzzle! solid throwback
  */
 class Dec19: PuzzleDayTester(19, 2024) {
 
-    override fun part1(): Any = loader().let { (towels, designs) ->
-        designs.count {
-            arrangeTowels(it, towels)
-        }
-    }
+    override fun part1(): Any = loader().let { (towels, designs) -> designs.count { arrangeTowels(it, towels) > 0 } }
+    override fun part2(): Any= loader().let { (towels, designs) -> designs.sumOf { arrangeTowels(it, towels) } }
 
-    fun arrangeTowels(design: String, towels: Set<String>): Boolean = computeIfAbsent(design) {
-        if (design.isEmpty()) {
-            true
-        } else {
-            towels.filter { design.startsWith(it) }.any {
-                arrangeTowels(design.drop(it.length), towels)
-            }
-        }
-    }
-
-    override fun part2(): Any {
-        return super.part2()
+    private fun arrangeTowels(design: String, towels: Set<String>): Long = computeIfAbsent(design) {
+        (1L).takeIf { design.isEmpty() } ?: towels.filter { design.startsWith(it) }.sumOf { arrangeTowels(design.drop(it.length), towels) }
     }
 
     private fun loader() = load(delimiter = "\n\n").let { (a, b) -> a.split(", ").toSet() to b.split("\n") }
 
-    private val memo = mutableMapOf<String, Boolean>()
+    private val memo = mutableMapOf<String, Long>()
 
-    private fun computeIfAbsent(key: String, compute: () -> Boolean): Boolean =
+    private fun computeIfAbsent(key: String, compute: () -> Long): Long =
         if (memo.containsKey(key)) {
             memo.getValue(key)
         } else {
