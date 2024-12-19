d4ad265d304c75fedbb66e0f92581cd37c47f039
Emery <memery@figure.com>
2024-12-19 09:03:52 -0700

    dec 19 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec19.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec19.kt
@@ -11,11 +11,11 @@ class Dec19: PuzzleDayTester(19, 2024) {
     override fun part1(): Any = loader().let { (towels, designs) -> designs.count { arrangeTowels(it, towels) > 0 } }
     override fun part2(): Any= loader().let { (towels, designs) -> designs.sumOf { arrangeTowels(it, towels) } }
 
-    private fun arrangeTowels(design: String, towels: Set<String>): Long = computeIfAbsent(design) {
+    private fun arrangeTowels(design: String, towels: List<String>): Long = computeIfAbsent(design) {
         (1L).takeIf { design.isEmpty() } ?: towels.filter { design.startsWith(it) }.sumOf { arrangeTowels(design.drop(it.length), towels) }
     }
 
-    private fun loader() = load(delimiter = "\n\n").let { (a, b) -> a.split(", ").toSet() to b.split("\n") }
+    private fun loader() = load(delimiter = "\n\n").let { (towels, designs) -> towels.split(", ") to designs.split("\n") }
 
     private val memo = mutableMapOf<String, Long>()
 
