13cf26946fde0a0412f69bdf67d7da85a6196e43
Emery <memery@figure.com>
2024-12-22 19:14:45 -0700

    dec 22 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec22.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec22.kt
@@ -7,6 +7,21 @@ import org.elwaxoro.advent.PuzzleDayTester
  */
 class Dec22: PuzzleDayTester(22, 2024) {
 
+    /**
+     * generate all the secrets, get the last one for each seed
+     */
+    override fun part1(): Any = load().sumOf { it.generateSecrets().last() }
+
+    override fun part2(): Any = load().flatMap { seed ->
+        val trimmed = seed.generateSecrets().map { it % 10 }
+        val diffs = listOf(trimmed.first()) + trimmed.zipWithNext { a, b -> b-a }
+        trimmed.zip(diffs).windowed(4, 1).map { chunk ->
+            chunk.joinToString(",") { "${it.second}" } to chunk.last().first
+        }.distinctBy { it.first }
+    }.groupBy { it.first }.map { it.value.sumOf { it.second } }.max()
+
+    private fun String.generateSecrets(): List<Long> = (1..2000).fold(mutableListOf(this.toLong())) { acc, _ -> acc.also { it.add(it.last().nextSecret()) }}
+
     private fun Long.nextSecret(): Long {
         var nextSecret = this.mix(this * 64).prune()
         nextSecret = nextSecret.mix(nextSecret / 32).prune()
@@ -16,22 +31,4 @@ class Dec22: PuzzleDayTester(22, 2024) {
 
     private fun Long.mix(that: Long): Long = this xor that
     private fun Long.prune(): Long = this % 16777216
-
-    override fun part1(): Any = load().sumOf { (1..2000).fold(it.toLong()) { acc, _ -> acc.nextSecret() } }
-
-    /**
-     * 2039 is too low
-     */
-    override fun part2(): Any = load().flatMap { startSecret ->
-        val secrets = (1..2000).fold(mutableListOf(startSecret.toLong())) { acc, _ -> acc.also { it.add(it.last().nextSecret()) }}
-        val trimmed = secrets.map { it % 10 }
-        val diff = listOf(trimmed.first()) + trimmed.zipWithNext { a, b -> b-a }
-        trimmed.zip(diff).windowed(4, 1).map { chunk ->
-            val key = chunk.joinToString(",") { "${it.second}" }
-            val value = chunk.last().first
-            key to value
-        }.distinctBy { it.first }
-    }.groupBy { it.first }.map {
-        it.value.sumOf { it.second }
-    }.max()
 }
