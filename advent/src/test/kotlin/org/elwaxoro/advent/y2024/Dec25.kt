06fc35113998874ecaddaa9bd2fd5c2764f2db3f
Emery <memery@figure.com>
2024-12-25 12:51:01 -0700

    dec 25 2024

--- advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec25.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2024/Dec25.kt
@@ -10,5 +10,7 @@ class Dec25 : PuzzleDayTester(25, 2024) {
 
     override fun part1(): Any = loader().let { parts -> parts.sumOf { a -> parts.filter { b -> a.none { b.contains(it) } }.size } / 2 }
 
+    override fun part2(): Any = "Merry Christmas!"
+
     private fun loader() = load(delimiter = "\n\n").let { l -> l.map { a -> a.split("\n").flatMapIndexed { y, s -> s.mapIndexedNotNull { x, c -> Coord(x, y).takeIf { c == '#' } } } } }
 }
