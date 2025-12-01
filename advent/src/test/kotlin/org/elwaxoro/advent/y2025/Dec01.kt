3d717eebd51ebdf3f69eafc6992891647ccdbcfb
Emery <memery@figure.com>
2025-12-01 13:19:13 -0700

    Dec 1 2025

--- advent/src/test/kotlin/org/elwaxoro/advent/y2025/Dec01.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2025/Dec01.kt
@@ -6,20 +6,20 @@ import kotlin.math.sign
 
 /**
  * Day 1: Secret Entrance
+ *
+ * Insight: it doesn't actually matter if the dial is kept strictly between 0-99, it can be negative as long as application of "% 100" is consistent
  */
 class Dec01 : PuzzleDayTester(1, 2025) {
 
-    override fun part1(): Any = loader().runningFold(50) { acc, i -> (acc + i) % 100 }.count { it == 0 } == 969
+    override fun part1(): Any = loader().spinDial(50).count { it == 0 } == 969
 
-    override fun part2(): Any = loader().let { input ->
-        var d = 50
-        input.flatMap { i ->
-            (1..abs(i)).map {
-                d = (d + i.sign) % 100
-                d
-            }
-        }.count { it == 0 }
-    } == 5887
+    override fun part2(): Any = loader().fold(50 to 0) { (dial, zeroes), i ->
+        List(abs(i)) { i.sign }.spinDial(dial).let { innerSpin ->
+            innerSpin.last() to (zeroes + innerSpin.drop(1).count { it == 0 })
+        }
+    }.second == 5887
+
+    private fun List<Int>.spinDial(initial: Int): List<Int> = runningFold(initial) { acc, i -> (acc + i) % 100 }
 
     private fun loader() = load().map { it.replace("R", "").replace("L", "-").toInt() }
 }
