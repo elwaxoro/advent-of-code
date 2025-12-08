0a7236e42c77b735065597f96ede69faa33634e0
Emery <memery@figure.com>
2025-12-08 10:39:36 -0700

    dec 8 2025

--- advent/src/test/kotlin/org/elwaxoro/advent/y2025/Dec08.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2025/Dec08.kt
@@ -14,31 +14,25 @@ class Dec08 : PuzzleDayTester(8, 2025) {
         (a to b) to a.distance(b)
     }.sortedBy { it.second }
 
-    override fun part1(): Any {
-        val sets = mutableListOf<Set<Coord3D>>()
-        distancePairs.take(1000).forEach { (pair, _) ->
-            sets.addMerge(pair)
-        }
-        return sets.sortedByDescending { it.size }.take(3).fold(1L) { acc, set -> acc * set.size } == 24360L
-    }
+    override fun part1(): Any =
+        distancePairs.take(1000)
+            .fold(mutableListOf<Set<Coord3D>>()) { sets, pair -> sets.addMerge(pair.first) }
+            .sortedByDescending { it.size }
+            .take(3)
+            .fold(1L) { acc, set -> acc * set.size } == 24360L
 
-    override fun part2(): Any = loader().let { junctions ->
-        val distancePairs = junctions.combinations(2).map { (a, b) ->
-            (a to b) to a.distance(b)
-        }.sortedBy { it.second }
+    override fun part2(): Any {
         val sets = mutableListOf<Set<Coord3D>>()
-        var last: Pair<Coord3D, Coord3D>
-        distancePairs.forEach { (pair, _) ->
+        var last: Pair<Coord3D, Coord3D> = (Coord3D() to Coord3D())
+        distancePairs.toList().takeWhile { (pair, _) -> 
             last = pair
             sets.addMerge(pair)
-            if (sets.first().size == junctions.size) {
-                return last.first.x.toLong() * last.second.x == 2185817796
-            }
+            sets.first().size != junctions.size
         }
-        return -1
+        return last.first.x.toLong() * last.second.x == 2185817796
     }
 
-    private fun MutableList<Set<Coord3D>>.addMerge(pair: Pair<Coord3D, Coord3D>) {
+    private fun MutableList<Set<Coord3D>>.addMerge(pair: Pair<Coord3D, Coord3D>): MutableList<Set<Coord3D>> {
         val r1 = filter { pair.first in it }
         val r2 = filter { pair.second in it }
         val r3 = listOf(setOf(pair.first, pair.second))
@@ -46,7 +40,8 @@ class Dec08 : PuzzleDayTester(8, 2025) {
         removeAll(r2)
         removeAll(r3)
         add((r1.flatten() + r2.flatten() + r3.flatten()).toSet())
+        return this
     }
 
-    private fun loader() = load().map { Coord3D.parse(it) }
+    private fun loader() = load().map(Coord3D::parse)
 }
